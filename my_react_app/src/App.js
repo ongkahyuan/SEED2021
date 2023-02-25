78c0c74afe88018a14f5113f3684fb87974fcf80
TEEKAYY <70378626+TsiKgan@users.noreply.github.com>
2023-02-25 13:58:53 +0800

    -
    -

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { Button } from './components/Button';
+import {BrowserRouter as Router, Routes, Route, Link, useNavigate} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Button/>
     </div>
   );
 }
---
+++ my_react_app/src/components/Button.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Modal from 'react-modal'
+import { DatePicker,Form, Input, Select, Space, Radio, Button as Btn , } from 'antd';
+
+
+export const Button = ()=>{
+    
+    // For Delete, Edit or Add
+    const [type, setType] = useState('')
+    const [open, setOpen] = useState(false);
+    const [errMsg, setErrMsg] = useState('')
+    const [claimNo, setClaimNo] = useState('')
+    const [claimAmount, setClaimAmount] = useState('')
+    const [purpose, setPurpose] = useState('')
+    const [date, setDate] = useState('')
+    const [followUp, setFollowUp] = useState(false)
+    const [previousClaimId, setPreviousClaimId] = useState('')
+    const { RangePicker } = DatePicker;
+    
+    useEffect(() => {
+      setType('Edit')
+    }, [])
+    
+    useEffect(()=>{
+      console.log({followUp})
+    },[setFollowUp])
+    const openModal = ()=> setOpen(true)
+
+    const closeModal = ()=>setOpen(false)
+
+  //   const deleteClaims = async(id) =>{
+        
+  //     try{
+  //        await axios.delete(`/api/${id}`,{
+  //            data:{
+  //                claimId:claimId
+  //            }
+  //        })
+  //        .then(response=>{ 
+  //            console.log(response.data)
+  //        })
+  //     } catch(err){
+  //        console.log(`Error:${err.messagae}`)
+  //     }
+  //     closeModal()
+  // }
+    const deleteClaims=()=>{
+      console.log('delete')
+    }
+    const editInputs= (e)=>{
+      
+      console.log({claimAmount})
+    }
+
+    const customStyles = {
+        content: {
+          width: '400px',
+          height: '100px',
+          margin: 'auto'
+        }
+      };
+
+      const onChange = (value, dateString) => {
+        console.log('Selected Time: ', value);
+        console.log('Formatted Selected Time: ', dateString);
+      };
+      const onOk = (value) => {
+        console.log('onOk: ', value);
+      };
+    return (
+      <div>
+      {
+      type==='Delete' 
+      ?  
+        <div>
+          <button onClick = {openModal}>{type}</button>
+            <Modal isOpen={open} onRequestClose={closeModal} style={customStyles} appElement={document.getElementById('root')}>
+                <div>
+                  <header><b>Are you sure you want to delete insurance claim No.?</b></header></div>
+                {/* <button onClick= {()=>(deleteClaims())}>Delete</button> */}
+                <Space wrap>
+                <div style={{ position: 'absolute', bottom: '5%', left: '45%', transform: 'translateX(-50%)' }}>
+                  <button  style ={{margin:'30px'}} onClick={deleteClaims}>Delete</button>
+                  <button onClick={closeModal}>Close Modal</button>
+                </div>
+                </Space>
+            </Modal>   
+        </div> 
+        :
+        <div>
+        <button onClick = {openModal}>{type}</button>
+        <Modal isOpen={open} onRequestClose={closeModal}  appElement={document.getElementById('root')}>
+            <h2>Edit Insurance Claim No:</h2>
+            <Form className="edit-form" onFinish = {editInputs}>
+                <div>
+                  <Form.Item label="Claim Amount">
+                  <Input value ={claimAmount} tpye="number"placeholder="$" onChange={(e)=>{setClaimAmount(e.target.value)}}/>
+                  </Form.Item>
+                </div>
+                <div>
+                  <Form.Item label="Purpose">
+                  <Input value ={purpose} tpye="number"placeholder="Purpose" onChange={(e)=>{setPurpose(e.target.value)}}/>
+                  </Form.Item>
+                </div>
+                {/* <div>
+                  <Form.Item label="Date">
+                  <Input value ={date} tpye="number"placeholder="DD/MM/YYYY" onChange={(e)=>{setDate(e.target.value)}}/>
+                  </Form.Item>
+                </div> */}
+                <Form.Item label="Date">
+                 <Space direction="vertical" size={12}>
+                 {/* <Input value ={date} onChange={(e)=>{setDate(e.target.value)}}/> */}
+                  <DatePicker showTime onChange={onChange} onOk={onOk} />
+                    <RangePicker
+                      // showTime={{
+                      //   format: 'HH:mm',
+                      // }}
+                      format="YYYY-MM-DD HH:mm"
+                      onChange={onChange}
+                      onOk={onOk}
+                    />
+                </Space>
+                </Form.Item>
+                <Form.Item label="Is this a follow up?" >
+                <Input 
+                type="checkbox" 
+                checked ={followUp}
+                value={followUp} 
+                onChange={(e)=>{setFollowUp(e.currentTarget.checked)}}
+                />
+                </Form.Item>
+                <br/>
+                <Space wrap>
+                <input type = "submit" value = 'Save' className="btn btn-block"></input>
+                {/* <input type = "submit" value = 'Update' className="btn btn-block" onClick={(console.log('asda'))}/> */}
+                <button onClick={closeModal}>Close Modal</button>
+                </Space>
+            </Form>
+        </Modal> 
+        </div>
+        
+        }
+      </div>
+      
+    )
+}
