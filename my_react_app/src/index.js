a8cbebebabdcea2f788c46cf0b7992bc02c0a3f0
GenesiaW <74142392+GenesiaW@users.noreply.github.com>
2023-02-25 12:03:43 +0800

    Add auth and browser router

--- my_react_app/src/index.js
+++ my_react_app/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from 'react-auth-kit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
-
-
